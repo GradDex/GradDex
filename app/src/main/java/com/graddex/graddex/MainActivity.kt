3a336ef638d9f7fa2ba3910c31e294b0ff4c4416
Mothana Dabash <Mothana@dabash.com.au>
2021-03-05 10:29:32 +1100

    added in PokemonResponse.kt

--- app/src/main/java/com/graddex/graddex/MainActivity.kt
+++ app/src/main/java/com/graddex/graddex/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.graddex.graddex
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import okhttp3.CacheControl
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
-import org.json.JSONObject
-
-
+import com.graddex.graddex.models.PokemonResponse
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.*
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,41 +17,55 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    val TAG = "test"
 
-    //get an instance of the client
-    private val client = OkHttpClient()
+    override fun onResume() {
+        super.onResume()
 
-    //build the request
-    val request = Request.Builder()
-        .cacheControl(CacheControl.Builder().noCache().build())
-        .url("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")
-        .build()
-
-    //execute
-    val response: Response = client.newCall(request).execute()
-
-    //call request
-    val call = client.newCall(request)
-
-    //extract JSON
-    val jsonDataString = response.body.toString()
-    val json: JSONObject = JSONObject(jsonDataString)
+        //get an instance of the client
+        val client = OkHttpClient()
+        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+       val adapter: JsonAdapter<PokemonResponse> = moshi.adapter(PokemonResponse::class.java)
 
 
-    /**
-    fun run() {
+        //build the request
         val request = Request.Builder()
             .cacheControl(CacheControl.Builder().noCache().build())
             .url("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")
             .build()
 
-        client.newCall(request).execute().use { response ->
-            if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
-            for ((name, value) in response.headers) {
-                println("$name: $value")
+        //execute
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                TODO("Not yet implemented")
             }
 
-            println(response.body!!.string())
-        } */
+            override fun onResponse(call: Call, response: Response) {
+
+                Log.d(TAG, "onResponse: ${response.code}")
+                val body = response.body?.string()
+                Log.d(TAG, "onResponse: $body")
+                val res = adapter.fromJson(body ?: "")
+                Log.d(TAG, "onResponse: ${res!!.results[0]}")
+
+            }
+        })
     }
+
+    /**
+    fun run() {
+    val request = Request.Builder()
+    .cacheControl(CacheControl.Builder().noCache().build())
+    .url("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")
+    .build()
+
+    client.newCall(request).execute().use { response ->
+    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+    for ((name, value) in response.headers) {
+    println("$name: $value")
+    }
+
+    println(response.body!!.string())
+    } */
+}
