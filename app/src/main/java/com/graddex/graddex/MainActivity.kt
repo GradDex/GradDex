6c8480d33c3c2ba48b89ad1652556564b09f8e50
Aniruddh Fichadia <aniruddh.fichadia@anz.com>
2021-03-05 12:52:25 +1100

    Creates a recyclerview to show pokemon
    - Updates MainActivity to have some basic loading, success, error states
    - Creates recyclerview to display pokemon list

--- app/src/main/java/com/graddex/graddex/MainActivity.kt
+++ app/src/main/java/com/graddex/graddex/MainActivity.kt
@@ -2,7 +2,13 @@ package com.graddex.graddex
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.graddex.graddex.models.PokemonResponse
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
@@ -13,11 +19,25 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG = "test"
+
+    private lateinit var statusText: TextView
+    private lateinit var pokemonRecyclerView: RecyclerView
+
+    private lateinit var pokemonAdapter: PokemonRecyclerAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        statusText = findViewById(R.id.status_text)
+        pokemonRecyclerView = findViewById(R.id.pokemon_recyclerview)
+        pokemonRecyclerView.layoutManager = LinearLayoutManager(this)
+        pokemonAdapter = PokemonRecyclerAdapter()
+        pokemonRecyclerView.adapter = pokemonAdapter
     }
-    val TAG = "test"
+
 
     override fun onResume() {
         super.onResume()
@@ -25,7 +45,7 @@ class MainActivity : AppCompatActivity() {
         //get an instance of the client
         val client = OkHttpClient()
         val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-       val adapter: JsonAdapter<PokemonResponse> = moshi.adapter(PokemonResponse::class.java)
+        val adapter: JsonAdapter<PokemonResponse> = moshi.adapter(PokemonResponse::class.java)
 
 
         //build the request
@@ -34,20 +54,27 @@ class MainActivity : AppCompatActivity() {
             .url("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")
             .build()
 
+        statusText.text = "Loading.."
         //execute
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
-                TODO("Not yet implemented")
+                runOnUiThread {
+                    statusText.text = "Error"
+                }
             }
 
             override fun onResponse(call: Call, response: Response) {
-
                 Log.d(TAG, "onResponse: ${response.code}")
                 val body = response.body?.string()
                 Log.d(TAG, "onResponse: $body")
-                val res = adapter.fromJson(body ?: "")
+                val res: PokemonResponse? = adapter.fromJson(body ?: "")
                 Log.d(TAG, "onResponse: ${res!!.results[0]}")
 
+                runOnUiThread {
+                    statusText.visibility = View.GONE
+                    pokemonRecyclerView.visibility = View.VISIBLE
+                    pokemonAdapter.submitList(res.results)
+                }
             }
         })
     }
---
+++ app/src/main/java/com/graddex/graddex/PokemonRecyclerAdapter.kt
@@ -0,0 +1,51 @@
+package com.graddex.graddex
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.graddex.graddex.models.PokemonResponse_Result
+
+class PokemonRecyclerAdapter
+    : ListAdapter<PokemonResponse_Result, PokemonViewHolder>(
+    object : DiffUtil.ItemCallback<PokemonResponse_Result>() {
+        override fun areItemsTheSame(
+            oldItem: PokemonResponse_Result,
+            newItem: PokemonResponse_Result
+        ): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(
+            oldItem: PokemonResponse_Result,
+            newItem: PokemonResponse_Result
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item_pokemon, parent, false)
+
+        return PokemonViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+class PokemonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val pokemonNameText: TextView = itemView.findViewById(R.id.pokemon_name)
+    private val pokemonUrlNameText: TextView = itemView.findViewById(R.id.pokemon_url)
+
+    fun bind(item: PokemonResponse_Result) {
+        pokemonNameText.text = item.name
+        pokemonUrlNameText.text = item.url
+    }
+}
