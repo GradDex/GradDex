4d2447c73f4a69e2c28e900f614e9f2b789e0cba
Lahiru Bandara <lahiru.bandara@gmail.com>
2021-03-22 16:20:11 +1100

    Enabled displaying of Pokemon details by clicking through RecyclerView list

--- app/src/main/java/com/graddex/graddex/PokemonDetailsFragment.kt
+++ app/src/main/java/com/graddex/graddex/PokemonDetailsFragment.kt
@@ -1,38 +1,51 @@
 package com.graddex.graddex
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.graddex.graddex.models.PokemonDetailsViewModel
 import com.graddex.graddex.databinding.FragmentPokemonDetailsBinding
+import com.graddex.graddex.models.PokemonDetailsResponse
 
 private val pokemonKey: String = "pokemonKey"
 
 class PokemonDetailsFragment(args: Bundle) : Fragment() {
 
-    private lateinit var viewModel: PokemonDetailsViewModel
-
     constructor(id: String) : this(bundleOf(pokemonKey to id))
-
     private val pokemonId: String = args.getString(pokemonKey, "")
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+            savedInstanceState: Bundle?): View? {
+
+        Log.d(tag, "PokemonID: $pokemonId")
 
         // Inflate the Pokemon Details fragment layout
         val binding = DataBindingUtil.inflate<FragmentPokemonDetailsBinding>(
                 inflater, R.layout.fragment_pokemon_details, container, false
         )
 
-        // Call the Pokemon Details view model
-        viewModel = ViewModelProvider(this).get(PokemonDetailsViewModel::class.java)
+        // Call the Pokemon Details view model for specified Pokemon ID
+        val viewModel: PokemonDetailsViewModel = ViewModelProvider(this)
+                .get(PokemonDetailsViewModel::class.java)
+
+        binding.statusText.text = getString(R.string.loading)
+        viewModel.syncPokemonDetails(pokemonId)
+        viewModel.pokemonDetails.observe(viewLifecycleOwner,
+            { pokemonDetails ->
+                binding.statusText.visibility = View.GONE
+                binding.pokemonName.visibility = View.VISIBLE
+                Log.d(tag, "pokemonDetails: $pokemonDetails")
+                binding.pokemonName.text = pokemonDetails.name
+                //binding.pokemonImage.visibility = View.VISIBLE
+            })
 
         return binding.root
 
--- app/src/main/java/com/graddex/graddex/models/PokemonDetailsViewModel.kt
+++ app/src/main/java/com/graddex/graddex/models/PokemonDetailsViewModel.kt
@@ -1,6 +1,46 @@
 package com.graddex.graddex.models
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.*
+import java.io.IOException
 
 class PokemonDetailsViewModel : ViewModel() {
+
+    val pokemonDetails: MutableLiveData<PokemonDetailsResponse> = MutableLiveData()
+
+    fun syncPokemonDetails(url: String) {
+
+        val tag = "PokeAPI Details"
+
+        // Initialise rest client and implement a JSON adapter
+        val client = OkHttpClient()
+        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+        val adapter: JsonAdapter<PokemonDetailsResponse> = moshi.adapter(
+                PokemonDetailsResponse::class.java)
+
+        // Build the request
+        val request = Request.Builder().url(url).build()
+
+        // Execute the request
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d(tag, "Call Failed")
+            }
+
+            // If request successfully gets a response
+            override fun onResponse(call: Call, response: Response) {
+                Log.d(tag, "Response: $response")
+                val body = response.body?.string()
+                Log.d(tag,"Body: $body")
+                val detailsRes = adapter.fromJson(body ?: "")
+                Log.d(tag, "detailsRes: ${detailsRes?.types}")
+                pokemonDetails.postValue(detailsRes)
+            }
+        })
+    }
 }
