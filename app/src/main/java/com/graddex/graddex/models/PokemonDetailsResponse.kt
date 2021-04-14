b7ec3d1ecba74d902200d79da12c3516e4a17c68
Aniruddh Fichadia <aniruddh.fichadia@anz.com>
2021-04-14 16:42:08 +1000

    Removes unnecessary moshi json annotations

--- app/src/main/java/com/graddex/graddex/models/PokemonDetailsResponse.kt
+++ app/src/main/java/com/graddex/graddex/models/PokemonDetailsResponse.kt
@@ -1,34 +1,17 @@
 package com.graddex.graddex.models
 
-import com.squareup.moshi.Json
+data class PokemonAbilities(val ability: PokemonResponseResult, val is_hidden: Boolean)
 
-data class PokemonAbilities(
-    @Json(name = "ability") val ability: PokemonResponseResult,
-    @Json(name = "is_hidden") val is_hidden: Boolean
-)
+data class PokemonTypes(val slot: Int, val type: PokemonResponseResult)
 
-data class PokemonTypes(
-    @Json(name = "slot") val slot: Int,
-    @Json(name = "type") val type: PokemonResponseResult,
-)
-
-data class PokemonSprites(
-    @Json(name = "back_default") val back_default: String,
-    @Json(name = "front_default") val front_default: String,
-)
+data class PokemonSprites(val back_default: String, val front_default: String)
 
 data class PokemonDetailsResponse(
-    @Json(name = "abilities") val abilities: List<PokemonAbilities>,
-    @Json(name = "name") val name: String,
-    @Json(name = "types") val types: List<PokemonTypes>,
-    @Json(name = "species") val species: PokemonResponseResult,
-    @Json(name = "sprites") val sprites: PokemonSprites
+    val abilities: List<PokemonAbilities>, val name: String, val types: List<PokemonTypes>,
+    val species: PokemonResponseResult, val sprites: PokemonSprites
 )
 
 data class PokemonDetails(
-    @Json(name = "frontSprite") var frontSprite: String,
-    @Json(name = "backSprite") val backSprite: String,
-    @Json(name = "name") val name: String,
-    @Json(name = "abilities") val abilities: List<String>,
-    @Json(name = "hiddenAbility") val hiddenAbility: String?
+        var frontSprite: String, val backSprite: String, val name: String, val abilities: List<String>,
+        val hiddenAbility: String?
 )
--- app/src/main/java/com/graddex/graddex/models/PokemonResponse.kt
+++ app/src/main/java/com/graddex/graddex/models/PokemonResponse.kt
@@ -1,11 +1,6 @@
 package com.graddex.graddex.models
 
-import com.squareup.moshi.Json
-
-data class PokemonResponseResult(
-    @Json(name = "name") val name: String,
-    @Json(name = "url") val url: String
-)
+data class PokemonResponseResult(val name: String, val url: String)
 
 data class PokemonResponse(
     val count: Int, val next: String?,
